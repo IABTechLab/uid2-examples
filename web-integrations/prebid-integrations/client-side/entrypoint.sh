a25f7a340c3359fd17a2ce8d9892086208115075
scott sundahl <scott.sundahl@thetradedesk.com>
2025-10-10 16:27:41 -0600

    naming for generic env variables

--- web-integrations/prebid-integrations/client-side/entrypoint.sh
+++ web-integrations/prebid-integrations/client-side/entrypoint.sh
@@ -4,7 +4,7 @@
 export $(cat /tmp/env | xargs)
 
 # Set default values if not provided
-export UID2_BASE_URL=${UID2_CSTG_BASE_URL:-"https://operator-integ.uidapi.com"}
+export BASE_URL=${UID2_CSTG_BASE_URL:-"https://operator-integ.uidapi.com"}
 export SERVER_PUBLIC_KEY=${UID2_CSTG_SERVER_PUBLIC_KEY:-"UID2-X-I-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEo+jcPlk8GWn3iG0R5Il2cbFQI9hR3TvHxaBUKHl5Vh+ugr+9uLMiXihka8To07ETFGghEifY96Hrpe5RnYko7Q=="}
 export SUBSCRIPTION_ID=${UID2_CSTG_SUBSCRIPTION_ID:-"DMr7uHxqLU"}
 
